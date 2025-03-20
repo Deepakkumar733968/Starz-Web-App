a4d6af0173f2798bf3010b9060b1e9e4e486f7f8
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-03-20 10:58:26 +0530

    changes

---
+++ my-app/src/starz-web-app/Series/page.tsx
@@ -0,0 +1,10 @@
+import "./style.css";
+const Series = () => {
+  return (
+    <>
+      <h1>Series</h1>
+    </>
+  );
+};
+
+export default Series;
--- my-app/src/starz-web-app/page.tsx
+++ my-app/src/starz-web-app/page.tsx
@@ -1,10 +1,12 @@
 import "./style.css";
 import Home from "./Home/page";
+import Series from "./Series/page";
 
 const StarzWebApp = () => {
   return (
     <main>
       <Home />
+      <Series />
     </main>
   );
 };
