bfc0014f39390b9aa4710b75d0e047f09cb236e0
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-03-20 20:36:35 +0530

    changes

--- my-app/src/starz-web-app/Series/TVShows/TVShows.tsx
+++ my-app/src/starz-web-app/Series/TVShows/TVShows.tsx
@@ -43,7 +43,7 @@ export const TVShows = () => {
   };
 
   return (
-    <section className="coming-soon-main">
+    <section className="shows-main">
       <div className="side-button-main">
         <div className="tv-shows-align-main">
           <div className="shows-header-align">
@@ -98,6 +98,61 @@ export const TVShows = () => {
           </div>
         </div>
       </div>
+      {/*  */}
+      {/* <div className="side-button-main">
+        <div className="tv-shows-align-main">
+          <div className="shows-header-align">
+            <a href="" className="remove-und">
+              <h2 className="originals-sup-header">Originals & Exclusives</h2>
+            </a>
+            <a href="" className="view-all-link">
+              View All
+            </a>
+          </div>
+          <div className="side-button-main">
+            <button
+              className="add-button-pad-one add-hover-arrow"
+              onClick={() => scroll("left")}
+            >
+              <MdArrowBackIos
+                className={showLeftArrow ? "button-icon" : "no-show-button"}
+              />
+            </button>
+            <div className="coming-image-card-main" ref={scrollRef}>
+              {ORIGINALS.map((item: ITVShows, index: number) => (
+                <ImageCard
+                  key={index}
+                  link={item.link}
+                  firstHeader={item.title}
+                  secondHeader={""}
+                  imageUrl={item.image}
+                  className=""
+                  headerStyle={{
+                    gap: "80px",
+                    width: "200px",
+                    top: "78px",
+                    left: "52px",
+                    marginLeft: "8px",
+                  }}
+                  imageSize={{ width: "325px", height: "181px" }}
+                  imageDivClass="today-div"
+                  imageClass="image-rad"
+                  firstHeaderClass="shows-title-header"
+                  secondHeaderClass=""
+                />
+              ))}
+            </div>
+            <button
+              className="add-button-pad-two add-hover-arrow"
+              onClick={() => scroll("right")}
+            >
+              <MdArrowForwardIos
+                className={showRightArrow ? "button-icon" : "no-show-button"}
+              />
+            </button>
+          </div>
+        </div>
+      </div> */}
     </section>
   );
 };
