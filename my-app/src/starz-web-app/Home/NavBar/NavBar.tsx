160692ee22a8cc474d8cc29514f9cea3ffa73c94
Deepakkumar <dkdeepakkumarind@gmail.com>
2025-03-21 07:47:23 +0530

    changes

--- my-app/src/starz-web-app/Home/NavBar/NavBar.tsx
+++ my-app/src/starz-web-app/Home/NavBar/NavBar.tsx
@@ -4,45 +4,47 @@ import { FiSearch } from "react-icons/fi";
 import { IoIosArrowDown } from "react-icons/io";
 import StarzButton from "../../../components/Button/StarzButton";
 
-export const NavBar = () => {
+export const NavBar = ({ isList = true }) => {
   return (
     <nav className="nav-main fade-in">
       <div className="nav-div-main">
-        <div className="main-nav-list">
-          <div>
-            <img
-              className="web-logo-nav"
-              src="https://www.starz.com/assets/images/icons/starz-logo-glint.svg"
-              alt="web-logo"
-            />
-          </div>
-          <Link to="/" className="nav-list">
-            HOME
-          </Link>
-          <Link to="/series" className="nav-list">
-            Series
-          </Link>
-          <Link to="/movies" className="nav-list">
-            Movies
-          </Link>
-          <Link to="/movies-detail" className="nav-list">
-            Schedule
-          </Link>
-          <div>
-            <a href="" className="nav-list search-main">
-              <span className="search-span">
-                <FiSearch className="search-icon" />
-                Browse
-              </span>
-              <span>
-                <IoIosArrowDown className="search-icon" />
-              </span>
+        <div className="logo-container">
+          <img
+            className="web-logo-nav"
+            src="https://www.starz.com/assets/images/icons/starz-logo-glint.svg"
+            alt="web-logo"
+          />
+        </div>
+        {isList && (
+          <div className="main-nav-list">
+            <Link to="/" className="nav-list">
+              HOME
+            </Link>
+            <Link to="/series" className="nav-list">
+              Series
+            </Link>
+            <Link to="/movies" className="nav-list">
+              Movies
+            </Link>
+            <Link to="/movies-detail" className="nav-list">
+              Schedule
+            </Link>
+            <div>
+              <a href="" className="nav-list search-main">
+                <span className="search-span">
+                  <FiSearch className="search-icon" />
+                  Browse
+                </span>
+                <span>
+                  <IoIosArrowDown className="search-icon" />
+                </span>
+              </a>
+            </div>
+            <a href="" className="nav-list">
+              #TakeTheLead
             </a>
           </div>
-          <a href="" className="nav-list">
-            #TakeTheLead
-          </a>
-        </div>
+        )}
         <div className="link-button-main">
           <a href="" className="nav-list login-list">
             <span>Log In</span>
--- my-app/src/starz-web-app/MoviesDetail/page.tsx
+++ my-app/src/starz-web-app/MoviesDetail/page.tsx
@@ -1,4 +1,5 @@
 import { Footer } from "../Home/Footer/Footer";
+import { NavBar } from "../Home/NavBar/NavBar";
 import { Cast } from "./Cast/Cast";
 import { DetailImgHeader } from "./DetailImgHeader/DetailImgHeader";
 import { GetEven } from "./GetEven/GetEven";
@@ -6,6 +7,7 @@ import { GetEven } from "./GetEven/GetEven";
 const MovieDetail = () => {
   return (
     <main>
+      <NavBar isList={false} />
       <DetailImgHeader />
       <Cast />
       <h1>Others Also Watched</h1>
